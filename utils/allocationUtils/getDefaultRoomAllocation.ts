612db478edc0430a3840d2c2b295cfd1838e2f35
Littlefish <berman82312@gmail.com>
2024-07-27 20:17:26 +0800

    Refactor: getDefaultRoomAllocation util

--- utils/allocationUtils/getDefaultRoomAllocation.ts
+++ utils/allocationUtils/getDefaultRoomAllocation.ts
@@ -5,6 +5,7 @@ import {
   type Room,
   type RoomAllocation,
 } from "@/types/RoomAllocation";
+import calculateRoomPrice from "./calculateRoomPrice";
 
 const ImpossiblePrice = Number.MAX_VALUE;
 
@@ -25,6 +26,9 @@ interface SolutionTable {
 
 const emptyAllocation = { adult: 0, child: 0, price: 0 };
 
+/**
+ * Utilize DP algorithm to find the best allocation
+ */
 export function getDefaultRoomAllocation(
   guest: Guest,
   rooms: Room[]
@@ -58,144 +62,125 @@ function createDpTable(adult: number, child: number) {
   return table;
 }
 
+/**
+ * Utilize DP algorithm to find the best allocation price.
+ * The solution might be more than one, with the same price.
+ */
 function findAllocations(
   solutionTable: SolutionTable,
   adult: number,
   child: number
 ) {
   if (adult === 0) {
-    //Should not happen, return empty possible result;
+    // Should not happen, return no possible result;
     return [];
   }
 
   if (solutionTable.solution[adult][child]) {
-    //Solved already, return solution
+    // Solved already, return solution
     return solutionTable.solution[adult][child];
   }
 
-  let price = ImpossiblePrice;
-  let allocations = [] as PossibleAllocations;
+  // Default possible solution is all guest in the same room.
+  let possibleNewSolutions: [PossibleAllocations, number][] = [
+    allocateSameRoom(solutionTable.rooms, adult, child),
+  ];
 
   if (adult > 1) {
+    // Find possible solution from adult - 1
     const previousAllocations = findAllocations(
       solutionTable,
       adult - 1,
       child
     );
-    previousAllocations.forEach((previousAllocation) => {
-      const [newAllocations, newPrice] = allocateAdult(
-        solutionTable.rooms,
-        previousAllocation
-      );
-      if (newPrice < price) {
-        price = newPrice;
-        allocations = newAllocations;
-      } else if (newPrice === price) {
-        allocations = mergePossibleAllocations(allocations, newAllocations);
-      }
-    });
+    possibleNewSolutions = possibleNewSolutions.concat(
+      previousAllocations.map((previousAllocation) => {
+        return allocateNewGuest(
+          solutionTable.rooms,
+          previousAllocation,
+          "adult"
+        );
+      })
+    );
   }
 
   if (child > 0) {
+    // Find possible solution form child - 1
     const previousAllocations = findAllocations(
       solutionTable,
       adult,
       child - 1
     );
-    previousAllocations.forEach((previousAllocation) => {
-      const [newAllocations, newPrice] = allocateChild(
-        solutionTable.rooms,
-        previousAllocation
-      );
-      if (newPrice < price) {
-        price = newPrice;
-        allocations = newAllocations;
-      } else if (newPrice === price) {
-        allocations = mergePossibleAllocations(allocations, newAllocations);
-      }
-    });
+    possibleNewSolutions = possibleNewSolutions.concat(
+      previousAllocations.map((previousAllocation) => {
+        return allocateNewGuest(
+          solutionTable.rooms,
+          previousAllocation,
+          "child"
+        );
+      })
+    );
   }
 
-  const [newAllocations, newPrice] = allocateSameRoom(
-    solutionTable.rooms,
-    adult,
-    child
+  // Dummy status: no solution
+  let price = ImpossiblePrice;
+  let allocations = [] as PossibleAllocations;
+
+  const newPrice = Math.min(
+    ...possibleNewSolutions.map((possibleSolution) => possibleSolution[1])
   );
 
   if (newPrice < price) {
-    allocations = newAllocations;
-    price = newPrice;
-  } else if (newPrice === price) {
-    allocations = mergePossibleAllocations(allocations, newAllocations);
+    // Get the cheapest solution(s).
+    const acceptedSolutions = possibleNewSolutions.filter(
+      (newSolution) => newSolution[1] === newPrice
+    );
+
+    // Merge all possible solutions and eliminate duplicated allocations.
+    acceptedSolutions.forEach((newAllocations) => {
+      allocations = mergePossibleAllocations(allocations, newAllocations[0]);
+    });
   }
 
   solutionTable.solution[adult][child] = allocations;
-
   return solutionTable.solution[adult][child];
 }
 
-function allocateAdult(
-  rooms: Room[],
-  previousAllocation: Allocations
-): [PossibleAllocations, number] {
-  const roomAddAdultPrices = fp
-    .zip(rooms, previousAllocation)
-    .map(([room, allocation]) => {
-      if (!room || !allocation) return ImpossiblePrice;
-      const currentPeople = allocation.adult + allocation.child;
-      const leftCapacity = room.capacity - currentPeople;
-      if (leftCapacity > 0) {
-        if (currentPeople === 0) {
-          return room.roomPrice + room.adultPrice;
-        }
+function addGuestPrice(
+  room: Room,
+  allocation: RoomAllocation,
+  guestType: "child" | "adult"
+) {
+  if (!room || !allocation) return ImpossiblePrice;
 
-        return room.adultPrice;
-      }
-
-      return ImpossiblePrice;
-    });
-
-  const minPrice = Math.min(...roomAddAdultPrices);
-
-  if (minPrice === ImpossiblePrice) {
-    return [[], ImpossiblePrice];
+  if (guestType === "child" && allocation.adult === 0) {
+    return ImpossiblePrice;
   }
 
-  const prePrice = previousAllocation.reduce((acc, cur) => acc + cur.price, 0);
-  const newPrice = prePrice + minPrice;
+  const currentPeople = allocation.adult + allocation.child;
+  const leftCapacity = room.capacity - currentPeople;
 
-  let allocations: PossibleAllocations = [];
-  roomAddAdultPrices.map((price, index) => {
-    if (price === minPrice) {
-      const newAllocation = fp.cloneDeep(previousAllocation);
-      newAllocation[index].adult += 1;
-      newAllocation[index].price += minPrice;
-      allocations.push(newAllocation);
+  if (leftCapacity > 0) {
+    const price = guestType === "child" ? room.childPrice : room.adultPrice;
+    if (currentPeople === 0) {
+      return room.roomPrice + price;
     }
-  });
+    return price;
+  }
 
-  return [allocations, newPrice];
+  return ImpossiblePrice;
 }
 
-function allocateChild(
+function allocateNewGuest(
   rooms: Room[],
-  previousAllocation: Allocations
+  previousAllocation: Allocations,
+  guestType: "child" | "adult"
 ): [PossibleAllocations, number] {
-  const roomAddChildPrices = fp
-    .zip(rooms, previousAllocation)
-    .map(([room, allocation]) => {
-      if (!room || !allocation) return ImpossiblePrice;
-      if (allocation.adult === 0) return ImpossiblePrice;
-      const currentPeople = allocation.adult + allocation.child;
-      const leftCapacity = room.capacity - currentPeople;
-      if (leftCapacity > 0) {
-        return room.childPrice;
-      }
+  const roomAddGuestPrices = rooms.map((room, index) =>
+    addGuestPrice(room, previousAllocation[index], guestType)
+  );
 
-      return ImpossiblePrice;
-    });
-
-  const minPrice = Math.min(...roomAddChildPrices);
+  const minPrice = Math.min(...roomAddGuestPrices);
 
   if (minPrice === ImpossiblePrice) {
     return [[], ImpossiblePrice];
@@ -205,10 +190,14 @@ function allocateChild(
   const newPrice = prePrice + minPrice;
 
   let allocations: PossibleAllocations = [];
-  roomAddChildPrices.map((price, index) => {
+  roomAddGuestPrices.map((price, index) => {
     if (price === minPrice) {
       const newAllocation = fp.cloneDeep(previousAllocation);
-      newAllocation[index].child += 1;
+      if (guestType === "adult") {
+        newAllocation[index].adult += 1;
+      } else {
+        newAllocation[index].child += 1;
+      }
       newAllocation[index].price += minPrice;
       allocations.push(newAllocation);
     }
@@ -236,7 +225,9 @@ function allocateSameRoom(
   adult: number,
   child: number
 ): [PossibleAllocations, number] {
-  const roomPrices = rooms.map((room) => allocatePrice(room, adult, child));
+  const roomPrices = rooms.map((room) =>
+    getRoomPriceForGuest(room, adult, child)
+  );
 
   const minPrice = Math.min(...roomPrices);
 
@@ -258,12 +249,12 @@ function allocateSameRoom(
   return [possibleSolutions, minPrice];
 }
 
-function allocatePrice(room: Room, adult: number, child: number) {
+function getRoomPriceForGuest(room: Room, adult: number, child: number) {
   if (room.capacity < adult + child) {
     return ImpossiblePrice;
   }
 
-  return room.roomPrice + adult * room.adultPrice + child * room.childPrice;
+  return calculateRoomPrice(room, { adult, child });
 }
 
 export default getDefaultRoomAllocation;
